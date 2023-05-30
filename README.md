6bfbbbbfbe4472d2bfeba860eeb091356c353577
aboud <gh.abd.com.207@gmail.com>
2023-05-30 14:45:59 +0300

    prepare to upload on streamlit add README file  and requrements file

---
+++ README.md
@@ -0,0 +1,2 @@
+this is simple project EDA Streamlit 
+
--- main.py
+++ main.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 # Load dataset
 df = pd.read_csv('Credit Score Classification Dataset.csv')
 
+# Color for Pie plot and Bar plot 
+colors = ['#7c90db', '#92a8d1', '#a5c4e1', '#f7cac9', '#fcbad3', '#e05b6f', '#f8b195', '#f5b971', '#f9c74f', '#ee6c4d', '#c94c4c', '#589a8e', '#a381b5', '#f8961e', '#4f5d75', '#6b5b95', '#9b59b6', '#b5e7a0', '#a2b9bc', '#b2ad7f', '#679436', '#878f99', '#c7b8ea', '#6f9fd8', '#d64161', '#f3722c', '#f9a828', '#ff7b25', '#7f7f7f']
+    
+
 for i in df.columns:
     try :       
         df[i] = df[i].astype(float)
@@ -15,9 +19,13 @@ for i in df.columns:
         continue
 
 # fucntion to plot barplot plotly 
-def plot(x,y):
+def plot(x,y,column):
     
-    fig = go.Figure(data =go.Bar(x = x , y = y))
+    fig = go.Figure(data =go.Bar(x = x , y = y,
+    marker=dict(
+                color=colors[:len(df[column].value_counts())],  
+                line=dict(color='black', width=2)
+            )))
 
     fig.update_layout(
         title = f'{x.name} VS {y.name}',
@@ -27,11 +35,6 @@ def plot(x,y):
     st.plotly_chart(fig)
 
 def subplot(df , column):
-
-    # Color for Pie plot and Bar plot 
-    colors = ['#a2b9bc', '#6b5b95', '#b2ad7f', '#feb236', '#b5e7a0', '#878f99',
-              '#d64161', '#86af49', '#ff7b25']
-    
 
     fig = make_subplots(rows=1, cols=2, subplot_titles=('Countplot', 'Bar'), specs=[[{"type": "xy"}, {'type': 'domain'}]])
 
@@ -77,17 +80,15 @@ def subplot(df , column):
                   template = 'plotly_dark')
     st.plotly_chart(fig)
     
+def Compare_columns_with_income(df):
+    column = st.selectbox('Selecte column you want to compare with Income' ,[ i for i in df.columns if i != 'Income'], 1 )
+    compare = df.groupby(column).sum()
+    columns_to_drop = [i for i in compare.columns if compare[i].dtype != 'float64']
+    compare = compare.drop(columns_to_drop , axis = 1 ).reset_index()
+    st.dataframe(compare)
+    plot(compare[column],compare['Income'],column)
+    subplot(df , column)
+    st.dataframe(df[column].value_counts())
 
-column = st.selectbox('Selecte column you want to compare with Income' , df.columns , 1 )
-# st.text(columns_to_drop)
-compare = df.groupby(column).sum()
-columns_to_drop = [i for i in compare.columns if compare[i].dtype != 'float64']
-
-compare = compare.drop(columns_to_drop , axis = 1 ).reset_index()
-st.dataframe(compare)
-
-plot(compare[column],compare['Income'])
-
-subplot(df , column)
-st.dataframe(df.Gender.value_counts())
-
+Compare_columns_with_income(df)
+st.checkbox('choose feature',)
